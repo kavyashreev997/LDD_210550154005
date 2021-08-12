78229e8d72d4184ac68f2ae4fb8dee1c04f7a7cf
kavyashreev997 <88151060+kavyashreev997@users.noreply.github.com>
2021-08-12 22:25:52 +0530

    code is updated

--- ASSIGNMENT-2/parameter3/addop.c
+++ ASSIGNMENT-2/parameter3/addop.c
@@ -3,45 +3,43 @@
 #include<linux/module.h>
 #include<linux/moduleparam.h>
  
-static int d1=5;
-static int d2=10;
-static char *opr = "add";
+static int x=20;
+static int y=10;
+static char* opr = "add";
 
-module_param(data, int, S_IRUSR|S_IWUSR);                     
-module_param(str, charp, S_IRUSR|S_IWUSR);                     
+ 
+module_param(x, int, S_IRUSR|S_IWUSR);
+module_param(y, int, S_IRUSR|S_IWUSR);
+module_param(opr, charp, S_IRUSR|S_IWUSR);                     
 
+ 
 
-
-static int __init hello_world_init(void)
+static int hello_world_init(void)
 {
-        int i;
-        printk("DATA = %d  \n",data);
-        printk("oprerand %s \n",opr);
-        switch(opr)
-        {
-            case "add":
-                     printk("ADD=%d",(d1+d2));
-                     break;
-            case "sub":
-                     printk("SUB=%d",(d1-d2));
-                     break;
-            case "mul":
-                     printk("MUL=%d",(d1*d2));
-                     break;
-            case "div":
-                     printk("DIV=%d",(d1/d2));
-                     break;
-           default:
-                     printk("ADD= %d",(d1+d2));
-                     break;
-                     
-        printk("Kernel Module Inserted Successfully...\n");
+        printk("operator = %s \n",opr);
+	if(!strcmp(opr,"add"))
+	{
+		printk("Add = %d",(x+y));
+	}
+	else if(!strcmp(opr,"sub"))
+	{
+		printk("Sub = %d",(x-y));
+	}
+	else if(!strcmp(opr,"mul"))
+	{	
+		printk("Mul = %d",(x*y));
+	}
+	else if(!strcmp(opr,"div"))
+	{	
+		printk("Div = %d",(x/y));
+	}
+        printk("In init module...\n");
     return 0;
 }
 
-static void __exit hello_world_exit(void)
+static void hello_world_exit(void)
 {
-    printk("Kernel Module Removed Successfully...\n");
+    printk("In exit module...\n");
 }
  
 module_init(hello_world_init);
